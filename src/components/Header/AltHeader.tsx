c11156679644b4f955042abac4cd8dc56d8edc97
jacobleesinger <jacobleesinger@gmail.com>
2021-04-14 17:53:18 -0700

    add signup page header logo + text

--- src/components/Header/AltHeader.tsx
+++ src/components/Header/AltHeader.tsx
@@ -5,20 +5,25 @@ import HeaderCenter from './HeaderCenter';
 import HeaderLeft from './HeaderLeft';
 import HeaderRight from './HeaderRight';
 import LinkTab from './LinkTab';
+import LightFont from '../Layout/LightFont';
 import Tab from './Tab';
 
 import './AltHeader.css';
+import logo from '../../assets/logo-white-background.png';
 
 const AltHeader: FC = () => {
   return (
     <Header className='alternate-header'>
       <HeaderLeft>
-        <LinkTab to='/route1'>Link 1</LinkTab>
-        <LinkTab to='/route2'>Link 2</LinkTab>
+        <Tab>HOME</Tab>
+        <Tab>ABOUT GAVIN</Tab>
+        <Tab>HOOLI-CON</Tab>
       </HeaderLeft>
-      <HeaderCenter />
+      <HeaderCenter>
+        <img src={logo} alt='Hooli' />
+      </HeaderCenter>
       <HeaderRight>
-        <Tab>Right side tab</Tab>
+        <Tab><LightFont>Making the world a better place.</LightFont></Tab>
       </HeaderRight>
     </Header>
   );
