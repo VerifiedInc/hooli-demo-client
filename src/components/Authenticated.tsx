d9982c61da67ad4581a676d783663539cee1d8db
Jacob Singer <jacobleesinger@gmail.com>
2021-05-13 18:43:35 -0700

    use first name from credential (#8)
    * [circleci, Dockerfile] use npm instead of yarn
    
    * [Authenticated] use updated images and display firstName from credential
    
    * [PrimaryHeader] use first name from credential in header

--- src/components/Authenticated.tsx
+++ src/components/Authenticated.tsx
@@ -8,6 +8,7 @@ import mobileImage from '../assets/hello-mobile.png';
 import { useIsMobile } from '../hooks/useIsMobile';
 
 import './Authenticated.css';
+import { isDemoPresentationDto } from '../typeguards';
 
 const Authenticated: FC = () => {
   const { sharedPresentation } = useTypedSelector(state => state.presentation);
@@ -17,11 +18,24 @@ const Authenticated: FC = () => {
     return <Redirect to='/' />;
   }
 
+  const credentials = isDemoPresentationDto(sharedPresentation)
+    ? sharedPresentation.presentation.verifiableCredential
+    : sharedPresentation.presentation.verifiableCredentials;
+
+  if (!credentials) {
+    return <Redirect to='/' />;
+  }
+
+  const credentialSubject = credentials[0].credentialSubject;
+
+  const firstName = typeof credentialSubject === 'string' ? JSON.parse(credentialSubject).firstName : credentialSubject.firstName;
+
   const image = isMobile ? mobileImage : desktopImage;
   return (
     <div className='authenticated'>
       <MainContent>
         {/* customize this with branding for the specific demo, better styling/layout/content, etc */}
+        <div className='hello'>Hello, {firstName}! <span role='img' aria-label='wave'>👋</span></div>
         <img src={image} alt='Hello, Richard!' />
       </MainContent>
     </div>
