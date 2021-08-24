4e8c5dd8a5f7b2fd59a05afe525d083f306802e8
jacobleesinger <jacobleesinger@gmail.com>
2021-08-23 18:03:54 -0700

    add logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './state';
+import LogRocket from 'logrocket';
 
+import { store } from './state';
 import App from './components/App';
+import { config } from './config';
+
 import './index.css';
+
+LogRocket.init(config.logRocketId);
 
 render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
